c2e61fdd08df5fb1bd569d3deef68ccf98a194c5
seohee <zawadi0805@gmail.com>
2021-08-13 08:35:16 +0900

    프로젝트 생성

---
+++ src/team2_project_menu_seohee/menu_seohee.java
@@ -0,0 +1,19 @@
+package team2_project_menu_seohee;
+
+public class menu_seohee {
+
+	int jajang;
+	int jjamppong;
+	int jajangCnt;
+	int jjamppongCnt;
+	
+	void jajangCntUp() {
+		for(jajang=0; jajang >= 100;  )
+		jajangCnt++;
+	}
+	void jjamppongCntUP() {
+		for(jjamppong=0; jjamppong >= 100; )
+		jjamppongCnt++;
+	}
+	
+}
