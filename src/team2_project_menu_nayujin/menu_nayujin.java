d72a2d0d342cb034d5a40cde6581a41dc0bd72c1
dbwls <youjin9874@naver.com>
2021-08-12 23:56:44 +0900

    계산기능 추가

--- src/team2_project_menu_nayujin/menu_nayujin.java
+++ src/team2_project_menu_nayujin/menu_nayujin.java
@@ -1,1 +1,25 @@
+package team2_project_Menu_nayujin;
 
+public class Menu_nayujin {
+	int 	black_Price=4500, spicy_Price=6000,
+			sum = black_Price+spicy_Price;
+	
+	
+	
+	void black_PriceUp(){
+		black_Price +=500;
+	}
+	void black_PriceDown(){
+		black_Price -=500;
+	}
+	
+	void spicy_PriceUp(){
+		black_Price +=500;
+	}
+	
+	void spicy_PriceDown(){
+		black_Price -=500;
+	}
+
+
+}
