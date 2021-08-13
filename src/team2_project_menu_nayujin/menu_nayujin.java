1b8f180e96b8d70903c74ff347b260e5f2f5774b
seohee <zawadi0805@gmail.com>
2021-08-13 12:25:22 +0900

    확인

--- src/team2_project_menu_nayujin/menu_nayujin.java
+++ src/team2_project_menu_nayujin/menu_nayujin.java
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 
-package team2_project_Menu_nayujin;
 
-public class menu_nayujin {
-	int 	black_Price=4500, spicy_Price=6000,
-			sum = black_Price+spicy_Price;
-=======
 package team2_project_menu_nayujin;
 
 public class menu_nayujin {
 	int 	black_Price=4500, spicy=6000,
 			sum = black_Price+spicy;
->>>>>>> master
 	
 	
 	
