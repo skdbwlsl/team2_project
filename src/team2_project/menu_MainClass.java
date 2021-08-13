6280998f8a403c29a9abc3c45444625d665d6400
dbwls <youjin9874@naver.com>
2021-08-13 14:36:35 +0900

    커밋을 몇번하는거야

--- src/team2_project/menu_MainClass.java
+++ src/team2_project/menu_MainClass.java
@@ -2,15 +2,8 @@ package team2_project;
 
 import java.util.Scanner;
 
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-=======
-
->>>>>>> master
-=======
->>>>>>> master
 import team2_project_jungyooseok.spicy_jungyooseok;
+import team2_project_menu_nayujin.menu_nayujin;
 import team2_project_seohee.count_seohee;
 
 public class menu_MainClass {
@@ -18,75 +11,80 @@ public class menu_MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		count_seohee cnt;
-		cnt = new count_seohee();
-
-
+		menu_nayujin mn = new menu_nayujin();
 		spicy_jungyooseok j = new spicy_jungyooseok();
-
+		count_seohee cnt = new count_seohee();
 		int num;
+
 		while(true) {
 		System.out.println("1.메뉴 선택");
 		System.out.println("2.소스, 맵기 선택");
-		System.out.println("3.금액 확인");
+		System.out.println("4.금액 확인");
 		System.out.println(">>>>>>");
 		num= input.nextInt();
 
-
-		switch(num) {
-		case 1 :
-			System.out.println("1.짜장면 2.짬뽕 ");
-			System.out.println(">>>>>>");
-			num= input.nextInt();
 			
-			if(num==1) {
-				System.out.println("1.짜장면 추가  2. 짜장면 취소");
-				 int ja = input.nextInt();
-				if(ja == 1) {
+			switch(num) {
+			case 1 :
+				System.out.println("1.짜장면 2.짬뽕 ");
+				System.out.println(">>>>>>");
+				num= input.nextInt();
+				if(num==1) {
+					System.out.println("짜장면 선택");
+				}else {
+					System.out.println("짬뽕 선택");
+				}
+	            break;
+	            
+			case 2 :
+				System.out.println("1. 맵기정도 UP, 2. 맵기정도 Down");
+				int vi = input.nextInt();
+				if(vi == 1) {
+					j.spicyUp();
+				}else if(vi  == 2 && j.spicy>0) {
+					j.spicyDown();
+				}
+				System.out.println("현재 맵기 단계 : "+j.spicy);
+				break;
+				
+			case 3 :
+				System.out.println("1. 짜장면 추가  2. 짜장면 취소 3. 짬뽕 추가 4. 짬뽕 취소");
+				System.out.println(">>");
+				int c = input.nextInt();
+
+				if(c == 1) {
 					cnt.jajangCntUp();
 					System.out.println("짜장면 갯수: " + cnt.jajangCnt);
 					System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
-				} else if(ja == 2 && cnt.jajangCnt>=0){
+				} else if(c == 2 && cnt.jajangCnt>=0){
 					cnt.jajangCntDown();
 					System.out.println("짜장면 갯수: " + cnt.jajangCnt);
 					System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
-				}
-			}else {
-				System.out.println("1. 짬뽕 추가  2. 짬뽕 취소");
-				int jjam = input.nextInt();
-				if(jjam == 1) {
+				} else if(c == 3) {
 					cnt.jjamppongCntUP();
 					System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
 					System.out.println("짜장면 갯수: " + cnt.jajangCnt);
-				} else if(jjam == 2 && cnt.jjamppongCnt>=0) {
+				} else if(c == 4 && cnt.jjamppongCnt>=0) {
 					cnt.jjamppongCntDown();
 					System.out.println("짬뽕 갯수: " + cnt.jjamppongCnt);
 					System.out.println("짜장면 갯수: " + cnt.jajangCnt);
 				} else {System.out.println("번호를 확인해주세요.");}
-			}
-			break;
 
-		case 2 :
-			System.out.println("1. 맵기정도 UP, 2. 맵기정도 Down");
-			int vi = input.nextInt();
-			if(vi == 1) {
-				j.spicyUp();
-			}else if(vi  == 2 && j.spicy>0) {
-				j.spicyDown();
-			}
-			System.out.println("현재 맵기 단계 : "+j.spicy);
-			break;
+				break;
 
+				
+			case 4 :
+				System.out.println("금액을 확인합니다");
+				System.out.println("총 금액은 ");
+			default : 
+				System.out.println("다시 선택하세요");    
+			
 
-
-		case 3 :
-			System.out.println("금액을 확인합니다");
-			System.out.println("총 금액은 " );
-		default : 
-			System.out.println("다시 선택하세요");    
-		}
+			
+		
 
 
+	
 
 		}
 
@@ -95,4 +93,4 @@ public class menu_MainClass {
 
 }
 
-
+}
